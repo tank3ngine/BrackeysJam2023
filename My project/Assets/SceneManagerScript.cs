20052dd61d4c516c76656160d5bfe8b9a04b032c
tank3ngine <117690704+tank3ngine@users.noreply.github.com>
2023-02-12 23:57:40 -0500

    Unity File added

---
+++ My project/Assets/SceneManagerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
