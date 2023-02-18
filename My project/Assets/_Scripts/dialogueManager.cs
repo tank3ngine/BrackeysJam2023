d5db0b87ad01f261ea6cd7942638b291faff08c2
Cythonic243 <98980373+Cythonic243@users.noreply.github.com>
2023-02-18 00:24:02 -0500

    DialgoueManager
    Didd dialogue manager for 1 scene and script just need to repeat for other scenes

---
+++ My project/Assets/_Scripts/dialogueManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro; 
+
+public class dialogueManager : MonoBehaviour
+{
+    [SerializeField] private string dialogueAssigner;
+
+    [SerializeField] public List<TMP_Text> Dialogue = new List<TMP_Text>();
+    [SerializeField] private TMP_Text dialogueText;
+
+    [SerializeField] private GameObject ExampleImage;
+
+    [SerializeField] private GameObject continueButton;
+    // [SerializeField] private GameObject nextSceneButton;
+
+    [SerializeField] private bool lastText = false;
+
+    [SerializeField] private int nextDialogueIndex = 0;
+
+
+     void Start()
+    {
+        dialogueText.text = "...";
+       // ExampleImage.SetActive(true);
+
+       // if (dialogueAssigner != "intro")
+       // {
+            //ExampleImage.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            //ExampleImage.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+       // }
+    }
+
+    public void ContinueButton()
+    {
+        Debug.Log("continueButton Pressed");
+        
+        dialogueText.text = Dialogue[nextDialogueIndex].text;
+        nextDialogueIndex++;
+        Debug.Log("index is: " + nextDialogueIndex);
+        if (nextDialogueIndex >= Dialogue.Count)
+
+        {
+            continueButton.SetActive(false);
+           // nextSceneButton.SetActive(true);
+         // Debug.Log("End of dialogue at index: " + nextDialoueIndex);
+
+        }
+
+        //checks dialogue sequence and what scene as 
+        //intro scene
+
+        if(dialogueAssigner == "intro")
+        {
+          //  if nextDialogueIndex == 2)
+            
+        }
+
+
+
+    }
+}
